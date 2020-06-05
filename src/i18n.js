78a4002b2cb8cbcd2a846bb114b18a515c56c306
Justin York <jyork@genward.com>
2020-06-05 14:38:36 -0600

    Add locale to gsp

--- src/i18n.js
+++ src/i18n.js
@@ -1,6 +1,8 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
+export const supportedLocales = ["en", "de", "es"];
+
 export default function initI18N(lang) {
   console.log("initI18N: lang:", lang);
   i18n.use(initReactI18next);
--- src/pages/[locale]/gsp/[id].js
+++ src/pages/[locale]/gsp/[id].js
@@ -1,3 +1,5 @@
+import { supportedLocales } from "../../../i18n";
+
 function GetStaticPathsPage({ id }) {
   return <h1>ID {id}</h1>;
 }
@@ -6,11 +8,16 @@ export default GetStaticPathsPage;
 
 export async function getStaticPaths() {
   return {
-    paths: [{ params: { id: "1" } }],
+    paths: supportedLocales.map((locale) => ({
+      params: {
+        id: "1",
+        locale,
+      },
+    })),
     fallback: true,
   };
 }
 
 export async function getStaticProps({ params }) {
-  return { props: { id: params.id } };
+  return { props: { id: params.id, locale: params.locale } };
 }
